306ddb54ac23bea5c8dacbb0aacbea16861f254e
Thomas Doornenbal <thomas.doornenbal@dotsafe.fr>
2018-10-29 22:17:27 +0100

    push component with dialogs

--- resources/assets/js/components/PushComponent.vue
+++ resources/assets/js/components/PushComponent.vue
@@ -1,57 +1,90 @@
 <template>
   <div>
-    <span v-if="displayButton" class="btn btn-default" @click="subscribe" :disabled="isButtonDisabled">
-      Activer les notifications
-    </span>
+    <aside id="push-component-dialog" class="mdc-dialog" aria-labelledby="push-component-dialog-label" aria-describedby="push-component-dialog-description">
+      <div class="mdc-dialog__surface">
+        <header class="mdc-dialog__header">
+          <h2 id="push-component-dialog-label" class="mdc-dialog__header__title">Notifications</h2>
+        </header>
+        <section id="push-component-dialog-description" class="mdc-dialog__body">
+          <p>Afin de recevoir les informations concernant vos clubs et événements, veuillez autoriser les notifications.</p>
+        </section>
+        <footer class="mdc-dialog__footer">
+          <button type="button" class="mdc-button mdc-dialog__footer__button mdc-dialog__footer__button--cancel" :disabled="isButtonDisabled">Fermer</button>
+          <button type="button" class="mdc-button mdc-dialog__footer__button mdc-dialog__footer__button--default" @click="subscribe" :disabled="isButtonDisabled">Autoriser les notifications</button>
+        </footer>
+      </div>
+      <div class="mdc-dialog__backdrop"></div>
+    </aside>
+
+    <aside id="push-component-fail-dialog" class="mdc-dialog" aria-labelledby="push-component-fail-dialog-label" aria-describedby="push-component-fail-dialog-description">
+      <div class="mdc-dialog__surface">
+        <header class="mdc-dialog__header">
+          <h2 id="push-component-fail-dialog-label" class="mdc-dialog__header__title">Notifications</h2>
+        </header>
+        <section id="push-component-fail-dialog-description" class="mdc-dialog__body">
+          <p>Vous avez bloqué les notifications mais il est encore possible de les débloquer.</p>
+        </section>
+        <footer class="mdc-dialog__footer">
+          <button type="button" class="mdc-button mdc-dialog__footer__button mdc-dialog__footer__button--cancel">Fermer</button>
+          <button type="button" class="mdc-button mdc-dialog__footer__button mdc-dialog__footer__button--default" @click="unblock">Comment faire ?</button>
+        </footer>
+      </div>
+      <div class="mdc-dialog__backdrop"></div>
+    </aside>
   </div>
 </template>
 
 <script>
+  import {MDCDialog} from '@material/dialog';
   export default {
     props: ['userId', 'pushSubscriptionEndpoints'],
     data () {
       return {
-        displayButton: false,
         isButtonDisabled: false
       }
     },
     mounted() {
-      var scope = this
-      this.showButton(Notification.permission !== 'granted')
+      let scope = this;
+      this.dialog = new MDCDialog(document.querySelector('#push-component-dialog'));
+      this.failDialog = new MDCDialog(document.querySelector('#push-component-fail-dialog'));
       if (this.pushSubscriptionEndpoints.length > 0) {
         navigator.serviceWorker.ready.then(function(registration) {
           registration.pushManager.getSubscription().then(function(subscription) {
-            if (JSON.parse(scope.pushSubscriptionEndpoints).indexOf(subscription.endpoint) === -1) {
-              scope.showButton(true)
+            if (subscription === null || subscription.endpoint === null || JSON.parse(scope.pushSubscriptionEndpoints).indexOf(subscription.endpoint) === -1) {
+              scope.dialog.show()
             }
           })
         })
       }
     },
     methods: {
-      showButton: function(value) {
-        this.displayButton = value
-      },
-
-      disableButton: function(value) {
+      disableButtons: function(value) {
         this.isButtonDisabled = value
       },
-      
+
       subscribe: function() {
-        var scope = this
-        this.disableButton(true)
+        let scope = this;
+        this.disableButtons(true);
         navigator.serviceWorker.ready.then(function(registration) {
           const subscribeOptions = {
             userVisibleOnly: true,
             applicationServerKey: scope.urlBase64ToUint8Array('BNp7AjH7Eh/pVxMJ9u+c3bLWKsrl7Hp9JyXUuMP479siq1qF+/AYCe0IJdO9s3ktfBYEmzROFvHtayt+CslwquA=')
-          }
+          };
           return registration.pushManager.subscribe(subscribeOptions)
         }).then(function(pushSubscription) {
           scope.sendSubscriptionToBackend(pushSubscription).then(function() {
-            scope.showButton(false)
-          })
+            scope.dialog.close()
+          });
           return pushSubscription;
+        }).catch(function() {
+          scope.dialog.close();
+          scope.failDialog.show();
+          return null;
         })
+      },
+
+      unblock: function() {
+        console.log('go to faq');
       },
 
       sendSubscriptionToBackend: function(subscription) {
